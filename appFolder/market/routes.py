8339282fb4ede234d35e817c2af131875f2578c4
Kaitlyn Huynh <kaitlynhuynh@10-18-134-135.dynapool.wireless.nyu.edu>
2024-04-18 18:49:03 -0400

    649

--- appFolder/market/routes.py
+++ appFolder/market/routes.py
@@ -1,10 +1,11 @@
 from market import app
 from flask import render_template, redirect, url_for, flash
 from market.models import Item, User
-# import market.forms
 from market.forms import RegisterForm, LoginForm,  FullNameForm
 from market import db
 from flask_login import login_user, logout_user, login_required
+
+
 
 @app.route('/')
 @app.route('/homepage')
