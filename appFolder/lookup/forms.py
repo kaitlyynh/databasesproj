183ec426a4ba5e40dc90201c3a53d24eac5c2868
Kaitlyn Huynh <kaitlynhuynh@Kaitlyns-MacBook-Pro.local>
2024-04-21 18:37:23 -0400

    removed unused icons

--- appFolder/lookup/forms.py
+++ appFolder/lookup/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField
 from wtforms.validators import Length, EqualTo, Email, DataRequired, ValidationError
 from lookup.models import User
 
